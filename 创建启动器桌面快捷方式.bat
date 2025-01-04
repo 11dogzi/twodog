34b7ce42fe740cb4ef08eb004bd5e50f4e673fc5
11dogzi <695660735@qq.com>
2025-01-04 08:35:55 +0800

    修正快捷方式创建脚本

--- 创建启动器桌面快捷方式.bat
+++ 创建启动器桌面快捷方式.bat
@@ -1,8 +1,13 @@
 @echo off
 chcp 65001 > nul
 setlocal
+
+:: 获取桌面路径
 for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop"') do set "DESKTOP_PATH=%%b"
-copy /Y "%~dp0Comfyui图狗启动器.exe" "%DESKTOP_PATH%\Comfyui图狗启动器.exe" > nul
+
+:: 创建快捷方式
+powershell.exe -Command "$WS = New-Object -ComObject WScript.Shell; $SC = $WS.CreateShortcut('%DESKTOP_PATH%\Comfyui图狗启动器.lnk'); $SC.TargetPath = '%~dp0Comfyui图狗启动器.exe'; $SC.WorkingDirectory = '%~dp0'; $SC.Save()"
+
 if %errorlevel% equ 0 (
     echo 快捷方式已创建成功！
 ) else (
